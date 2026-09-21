63d15b62b41b52ae3a5843be1042a1398f7e4893
Ganat Ahmed <ganata428@gmail.com>
2026-09-21 22:09:25 +0300

    Finish Week 15 challenges and assignments

--- BOM-02/Week-15/assignments/task-15.js
+++ BOM-02/Week-15/assignments/task-15.js
@@ -1,2 +1,37 @@
 // task-1
+let font = document.querySelector("#font");
+let color = document.querySelector("#color");
+let size = document.querySelector("#size");
+let text = document.querySelector("#text");
+
+if (localStorage.getItem("font")) {
+  text.style.fontFamily = localStorage.getItem("font");
+  font.value = localStorage.getItem("font");
+}
+
+if (localStorage.getItem("color")) {
+  text.style.color = localStorage.getItem("color");
+  color.value = localStorage.getItem("color");
+}
+
+if (localStorage.getItem("size")) {
+  text.style.fontSize = localStorage.getItem("size");
+  size.value = localStorage.getItem("size");
+}
+
+font.onchange = function () {
+  text.style.fontFamily = this.value;
+  localStorage.setItem("font", this.value);
+};
+
+color.onchange = function () {
+  text.style.color = this.value;
+  localStorage.setItem("color", this.value);
+};
+
+size.onchange = function () {
+  text.style.fontSize = this.value;
+  localStorage.setItem("size", this.value);
+};
+
 // task-2
